6e55f0138ad0bd77fa5357c2ae6f3207b9353f93
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-17 12:56:24 -0500

    Bug Fix for Lab 04g

--- lab04/src/main/scala/Driver.scala
+++ lab04/src/main/scala/Driver.scala
@@ -54,7 +54,7 @@ object Lab04 {
 		val triangle = extended_edge_decrease.join(two_edge)
 
 		// Aggregate Edges by Node ID
-		// FIXME: Maybe I should use join here...?
+		// FIXME: Maybe I should use join here...
 		val edgeAgg = triangle.map(x => (x._2._2, x._1)).distinct().groupByKey.mapValues(_.toList)
 
 		// Compute Triangle Per Node
@@ -65,11 +65,11 @@ object Lab04 {
 
 		// Aggregate and Compute Clustering Coefficient
 		// (Node_ID, (Triangle Count, Neighbor Count))
-		val result = triCount.join(nodeCount).sortByKey(false)
+		val result = triCount.join(nodeCount).sortByKey(false).map(x => (x._1, x._2._1, x._2._2)).collect()
 
 		// Generate Output File
         val writer = new PrintWriter(new File("output.txt"))
-        result.foreach(x => writer.write(x._1 + "\t" + x._2._1 + "\t" + x._2._2 + "\n"))
+        result.foreach(x => writer.write(x._1 + "\t" + x._2 + "\t" + x._3 + "\n"))
         writer.close()
     }
 }
