8edc252a13c3963a2052199fa94149b6fbe876f5
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-19 01:03:37 -0500

    update algorithm

--- lab04/src/main/scala/Driver.scala
+++ lab04/src/main/scala/Driver.scala
@@ -1,5 +1,5 @@
 /*
- * DS410 - Lab 04: Network Clustering Coefficient
+ * DS410 - Lab 04: Network Cluster Coefficient
  * Author: Yuya Jeremy Ong (yjo5006@psu.edu)
  */
 import java.io.PrintWriter
@@ -16,7 +16,7 @@ import scala.io.Source
 import scala.util.Try
 
 object Lab04 {
-    // Application Specific Variables
+    	// Application Specific Variables
 	private final val SPARK_MASTER = "yarn-client"
 	private final val APPLICATION_NAME = "lab04"
 
@@ -24,56 +24,48 @@ object Lab04 {
 	private final val CORE_SITE_CONFIG_PATH = new Path("/usr/hdp/current/hadoop-client/conf/core-site.xml")
 	private final val HDFS_SITE_CONFIG_PATH = new Path("/usr/hdp/current/hadoop-client/conf/hdfs-site.xml")
 
-    def main(args: Array[String]): Unit = {
-        // Configure SparkContext
+    	def main(args: Array[String]): Unit = {
+        	// Configure SparkContext
 		val conf = new SparkConf().setMaster(SPARK_MASTER).setAppName(APPLICATION_NAME)
 		val sc = new SparkContext(conf)
 
-        // Configure HDFS
+	        // Configure HDFS
 		val configuration = new Configuration();
 		configuration.addResource(CORE_SITE_CONFIG_PATH);
 		configuration.addResource(HDFS_SITE_CONFIG_PATH);
 
-        // Parse Lines, Filter and Extract Tokens
+        	// Parse Lines, Filter and Extract Tokens
 		val lines = sc.textFile("hdfs:/ds410/lab4/CSNNetwork.csv")
 		val item  = lines.map(line => line.split(","))
 		val string_item = item.filter( i => Try(i(1).toInt).isSuccess && Try(i(2).toInt).isSuccess)
 
-		// Parse Edge ID Values
+		// Convert to Vertex Pair
 		val int_item = string_item.map( cs => (cs(1).toInt, cs(2).toInt)).filter( cs => cs._1 != cs._2)
 
-		// Sort Edge Values
+		// Permute Edge Ordering
 		val edge_increase = int_item.map(cs => (if (cs._1 < cs._2) (cs._1, cs._2); else (cs._2, cs._1))).distinct()
 		val edge_decrease = int_item.map(cs => (if (cs._1 < cs._2) (cs._2, cs._1); else (cs._1, cs._2))).distinct()
 
-		// Join Edge Inc & Dec
-		val two_edge = edge_increase.join(edge_decrease).map(cs => (cs._2, cs._1))
-
 		// Formulate Triangles
+		val two_edge = edge_increase.join(edge_decrease).map( cs => (cs._2, cs._1))
 		val extended_edge_decrease = edge_decrease.map(cs => (cs, 1))
 		val triangle = extended_edge_decrease.join(two_edge)
 
-		// Aggregate Edges by Node ID
-		val edgeAgg = triangle.map(x => (x._2._2, x._1)).distinct().groupByKey.mapValues(_.toList)
+		// Generate Triangle Tuples
+		val triTuple = triangle.map(x=>(x._1._1,x._1._2,x._2._2))
+		val triCount = triTuple.flatMap(t => List(t._1, t._2, t._3)).groupBy(identity).mapValues(_.size)
 
-		// Compute Triangle Per Node
-		val triCount = edgeAgg.map(x => (x._1, x._2.length))
+		// Compute Per Node Neighbor Counts
+		val inc_group = edge_inc.groupByKey.mapValues(_.toList)
+		val dec_group = edge_dec.groupByKey.mapValues(_.toList)
+		val nodeCount = inc_group.union(dec_group).groupByKey.mapValues(_.flatMap(x=>x).toList.length)
 
-		// Count List of Neighbors Per Node
-		val inc_group = edge_increase.groupByKey.mapValues(_.toList)
-		val dec_group = edge_decrease.groupByKey.mapValues(_.toList)
+		// Compute Network Cluster Coefficient
+		val results = triCount.fullOuterJoin(nodeCount).mapValues(x => if (x._1 != None) (x._1.get, x._1.get / (x._2.get * (x._2.get-1) / 2.0).toDouble) else (0, 0)).collect()
 
-		// Merge Increase and Decrease Edges and Get Total Distinct Counts
-		val nodeCount = inc_group.join(dec_group).mapValues(x => (x._1 ++ x._2).distinct.length )
-
-		// Aggregate and Compute Clustering Coefficient
-		// (Node_ID, (Neighbor Count, Triangle Count))
-
-		val result = nodeCount.join(triCount).sortByKey().map(x => (x._1, x._2._1, (x._2._1 / ((x._2._2 * (x._2._2 - 1)) / 2.0).toDouble))).collect()
-
-		// Generate Output File
-        val writer = new PrintWriter(new File("output.txt"))
-        result.foreach(x => writer.write(x._1 + "\t" + x._2 + "\t" + x._3 + "\n"))
-        writer.close()
+        	// Generate Output File
+        	val writer = new PrintWriter(new File("output.txt"))
+        	top100.foreach(x => writer.write(x._1 + "\t" + x._2._1 + "\t" + x._2._2 + "\n"))
+        	writer.close()
     }
 }
