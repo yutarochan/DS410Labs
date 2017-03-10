f6a75c40b5e393ee839289b9df09ff69ca64e360
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-03-09 23:31:44 -0500

    (fix) Fix type mismatch

--- lab06/src/main/scala/KMeans.scala
+++ lab06/src/main/scala/KMeans.scala
@@ -70,6 +70,7 @@ class Kmeans (val k:Int, val f:Int) extends java.io.Serializable{
                 }
             }
             val new_clusters_list = new_clusters.map(s => (s._1, s._2.toList))
+            println(new_clusters)
             return new_clusters_list
     }
 
