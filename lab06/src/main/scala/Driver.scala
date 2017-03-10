f8f580bd330c269408ded787c759ac326c6831ff
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-03-09 22:49:57 -0500

    (fix) Fix typo

--- lab06/src/main/scala/Driver.scala
+++ lab06/src/main/scala/Driver.scala
@@ -45,7 +45,7 @@ object Lab05 {
 
         // Perform K-Means
         val k = new Kmeans(3, 4)
-        k.run_for(samples, 100)
+        k.for_run(samples, 100)
 
         val centers = k.centers
 
