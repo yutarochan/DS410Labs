1475b161a855377afb9756e81d2c817ee67ffe98
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-03-30 00:14:06 -0400

    (fix) Update memory for spark-submit

--- lab07/execute.sh
+++ lab07/execute.sh
@@ -1,1 +1,1 @@
-spark-submit --master yarn-client --driver-memory 10g --executor-memory 10g --num-executors 5 --executor-cores 16 target/scala-2.10/lab7_2.10-1.0.jar  kmeans
+spark-submit --master yarn-client --driver-memory 15g --executor-memory 15g --num-executors 5 --executor-cores 16 target/scala-2.10/lab7_2.10-1.0.jar  kmeans
